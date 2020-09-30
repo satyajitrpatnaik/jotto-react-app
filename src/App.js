e5d1ad57c17f51165b3a9a1b3c1e16acc9891fbe
satyajitrpatnaik <bvr.satyajit.patnaik@gmail.com>
2020-09-30 18:55:48 +0530

    Mock getSecretWord calls in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
 import GuessedWords from './components/GuessedWords/GuessedWords';
 import Congrats from './components/Congrats/Congrats';
+import { getSecretWord } from './store/secretWord/secretWordActionCreators';
 
-class App extends Component {
+export class UnconnectedApp extends Component {
+
+  componentDidMount() {
+    this.props.getSecretWord();
+  }
+
   render() {
     return (
       <div className="container">
         <h1>Jotto</h1>
-        <Congrats success={true}/>
-        <GuessedWords guessedWords={[
-          { guessedWord: 'train', letterMatchCount: 1 }
-        ]}/>
+        <Congrats success={this.props.success}/>
+        <GuessedWords guessedWords={this.props.guessedWords}/>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    secretWord: state.secretWord,
+    guessedWords: state.guessedWords,
+    success: state.success
+  }
+};
+
+export default connect(mapStateToProps, { getSecretWord })(UnconnectedApp);
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,61 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { storeFactory } from './test/testUtils';
+
+import App, { UnconnectedApp } from './App';
+
 describe('<App />', () => {
-  it('placeholder test', () => {
-    expect(true).toBeTruthy();
+  
+  const setup = (state = {}, props) => {
+    const store = storeFactory(state);
+    const wrapper = shallow(<App store={store}/>).dive().dive();
+    return wrapper;
+  };
+
+  describe('redux props', () => {
+    it('has access to success props', () => {
+      const success = true;
+      const wrapper = setup({ success });
+      const successProp = wrapper.instance().props.success;
+      expect(successProp).toBe(success);
+    });
+
+    it('has access to secretWord prop', () => {
+      const secretWord = 'party';
+      const wrapper = setup({ secretWord });
+      const secretWordProp = wrapper.instance().props.secretWord;
+      expect(secretWordProp).toBe(secretWord);
+    });
+
+    it('has access to guessedWords prop', () => {
+      const guessedWords = [{ guessedWord: 'train', letterMatchCount: 3 }];
+      const wrapper = setup({ guessedWords });
+      const guessedWordsProp = wrapper.instance().props.guessedWords;
+      expect(guessedWordsProp).toEqual(guessedWords);
+    });
+
+    it('has `getSecretWord` action creator of Function type', () => {
+      const wrapper = setup();
+      const getSecretWordProp = wrapper.instance().props.getSecretWord;
+      expect(getSecretWordProp).toBeInstanceOf(Function);
+    });
   });
+
+  describe('getSecretWord', () => {
+    it('runs on App mount', () => {
+      const getSecretWordMock = jest.fn();
+
+      const props = {
+        success: false,
+        getSecretWord: getSecretWordMock,
+        guessedWords: []
+      }
+      const wrapper = shallow(<UnconnectedApp {...props}/>);
+      wrapper.instance().componentDidMount();
+      const getSecretWordCallCount = getSecretWordMock.mock.calls.length;
+      expect(getSecretWordCallCount).toBe(1);
+    });
+  });
+  
 });
 
