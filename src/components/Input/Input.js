52e5c5f4b948cc1e1f6235f030fcd9971391d2e7
satyajitrpatnaik <bvr.satyajit.patnaik@gmail.com>
2020-10-04 21:48:59 +0530

    Submitting and Clearing Input Field Value

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,9 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = ({ secretWord }) => {
+
+  const [ currentGuess, setCurrentGuess ] = React.useState('');
+
   return (
     <div data-test="component-input">
-
+      <form className="form-inline">
+        <input data-test="input-box" className="mb-2 mx-sm-3" type="text" placeholder="enter guess"
+          onChange={(event) => {
+            setCurrentGuess(event.target.value);
+          }}
+          value={currentGuess}/>
+        <button data-test="submit-button" className="btn btn-primary mb-2" type="submit"
+          onClick={(event) => {
+            event.preventDefault();
+            setCurrentGuess('');
+          }}>Submit</button>
+      </form>
     </div>
   )
 };
@@ -14,21 +28,3 @@ Input.propTypes = {
 }
 
 export default Input;
-
-/* 
-render() {
-  const contents = this.props.success ? null : (
-    <form className="form-inline">
-      <input data-test="input-box" className="mb-2 mx-sm-3" type="text" placeholder="enter guess"
-        onChange={(event) => this.setState({ currentGuess: event.target.value })}
-        value={this.state.currentGuess}/>
-      <button data-test="submit-button" className="btn btn-primary" type="submit"
-        onClick={this.submitGuessedWord}>Submit</button>
-    </form>
-  )
-  return (
-    <div data-test="component-input">
-      { contents }
-    </div>
-  )
-} */
--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -23,6 +23,36 @@ describe('<Input />', () => {
   it('does not throw any warnings with expected props', () => {
     checkProps(Input, { secretWord: 'party' });
   });
+
+  describe('state controlled input field', () => {
+    
+    let mockSetCurrentGuess = jest.fn();;
+    let wrapper;
+
+    beforeEach(() => {
+      mockSetCurrentGuess.mockClear();
+      React.useState = jest.fn(() => [ '', mockSetCurrentGuess ]);
+      wrapper = setup();
+    });
+
+    it('state updates with value of input box upon change', () => {
+      
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      const mockEvent = { target: { value: 'train' }};
+      inputBox.simulate('change', mockEvent);
+
+      expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+    });
+
+    it('field is cleared upon submit button click', () => {
+      const mockSetCurrentGuess = jest.fn();
+      React.useState = jest.fn(() => [ '', mockSetCurrentGuess ]);
+      const wrapper = setup();
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      submitButton.simulate('click', { preventDefault() {} });
+      expect(mockSetCurrentGuess).toHaveBeenCalledWith('');
+    });
+  });
   
 });
 
