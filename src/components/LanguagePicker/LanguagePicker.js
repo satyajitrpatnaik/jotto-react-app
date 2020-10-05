b8665ac10585722997962e90257620fb96feb7ab
satyajitrpatnaik <bvr.satyajit.patnaik@gmail.com>
2020-10-05 15:24:16 +0530

    Add tests for Language Picker

--- src/components/LanguagePicker/LanguagePicker.js
+++ src/components/LanguagePicker/LanguagePicker.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function LanguagePicker({ setLanguage }) {
+
+  const languages = [
+    { code: 'en', symbol: '🇺🇸' },
+    { code: 'emoji', symbol: '😊' }
+  ];
+
+  const languageIcons = languages.map(lang => {
+    return (
+      <span data-test="language-icon" key={lang.code}
+        onClick={() => setLanguage(lang.code)}>
+        {lang.symbol}
+      </span>
+    );
+  });
+
   return (
-    <div />
+    <div data-test="component-language-picker">
+      { languageIcons }
+    </div>
   );
+}
+
+LanguagePicker.propTypes = {
+  setLanguage: PropTypes.func.isRequired
 }
 
 export default LanguagePicker;
--- src/components/LanguagePicker/LanguagePicker.test.js
+++ src/components/LanguagePicker/LanguagePicker.test.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import { findByTestAttr, checkProps } from './../../test/testUtils';
+import LanguagePicker from './LanguagePicker';
 
 describe('<LanguagePicker />', () => {
-  it('renders without errors', () => {
 
+  const mockSetLanguage = jest.fn();
+
+  const setup = () => {
+    return shallow(<LanguagePicker setLanguage={mockSetLanguage}/>);    
+  };
+
+  it('renders without errors', () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, 'component-language-picker');
+    expect(component.length).toBe(1);
   });
 
   it('does not throw any warnings with expected props', () => {
-
+    const wrapper = setup();
+    checkProps(LanguagePicker, { setLanguage: jest.fn() });
   });
 
   it('calls the setLanguage prop upon click', () => {
-
+    const wrapper = setup();
+    const languageIcons = findByTestAttr(wrapper, 'language-icon');
+    const firstIcon = languageIcons.first();
+    firstIcon.simulate('click');
+    expect(mockSetLanguage).toHaveBeenCalled();
   });
 
   it('renders non-zero language icons', () => {
-
+    const wrapper= setup();
+    const languageIcons = findByTestAttr(wrapper, 'language-icon');
+    expect(languageIcons.length).toBeGreaterThan(0);
   });
-  
+
 });
